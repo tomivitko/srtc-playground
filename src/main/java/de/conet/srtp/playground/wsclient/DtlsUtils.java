d8855362071b34801a3e2960ac2f48f581d38dec
Nastic <DNastic@conet.hr>
2022-06-20 15:46:03 +0200

    added use of RTPConnector

--- src/main/java/de/conet/srtp/playground/wsclient/DtlsUtils.java
+++ src/main/java/de/conet/srtp/playground/wsclient/DtlsUtils.java
@@ -1,18 +1,23 @@
 package de.conet.srtp.playground.wsclient;
 
-import org.bouncycastle.tls.DTLSProtocol;
+import java.io.IOException;
+import java.net.InetAddress;
+import javax.media.rtp.SessionAddress;
 import org.jitsi.impl.neomedia.NeomediaServiceUtils;
+import org.jitsi.impl.neomedia.RTPConnectorUDPImpl;
 import org.jitsi.impl.neomedia.transform.dtls.DtlsControlImpl;
 import org.jitsi.impl.neomedia.transform.dtls.DtlsPacketTransformer;
 import org.jitsi.impl.neomedia.transform.dtls.DtlsTransformEngine;
 import org.jitsi.service.libjitsi.LibJitsi;
+import org.jitsi.service.neomedia.DefaultStreamConnector;
 import org.jitsi.service.neomedia.SrtpControl;
 import org.jitsi.service.neomedia.SrtpControlType;
+import org.jitsi.service.neomedia.StreamConnector;
 import net.sf.fmj.media.Log;
 
 public class DtlsUtils {
 
-    public void connect(DTLSProtocol dtlsProtocol) {
+    public void connect(Integer port) {
         /*        try {
             if (dtlsProtocol instanceof DTLSClientProtocol) {
                 ((DTLSClientProtocol) dtlsProtocol).connect(null, null);
@@ -32,5 +37,19 @@ public class DtlsUtils {
         byte[] buff = {1};
         Log.info("\n\n\n sending dtls packet\n\n\n\n");
         dtlsPacketTransformer.sendApplicationData(buff, 0, 1);
+
+        StreamConnector streamConnector = new DefaultStreamConnector();
+
+        RTPConnectorUDPImpl rtpConnectorUDPImpl = new RTPConnectorUDPImpl(streamConnector);
+        InetAddress inetAddress;
+        try {
+            inetAddress = InetAddress.getByName("localhost");
+            SessionAddress address = new SessionAddress(inetAddress, port);
+            rtpConnectorUDPImpl.addTarget(address);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException("Unchecked exception occurs: " + e.toString(), e);
+        }
+
     }
 }
